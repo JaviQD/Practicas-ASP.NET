a8f6f473c47fe23eadccfad414496f32600d27c1
Javi Quintana <rikiquintana7@gmail.com>
2024-05-05 01:05:09 -0500

    Clean Code

--- Controllers/AuthRegistroController.cs
+++ Controllers/AuthRegistroController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.CodeAnalysis.Scripting;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Practicas_ASP.NET.Methods;
 using Practicas_ASP.NET.Models;
